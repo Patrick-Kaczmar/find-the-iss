3681297291c15bc10ea78b1fabf9a003b8bbaa4f
Patrick Kaczmar <pk1blue@yahoo.com>
2022-07-10 23:33:18 -0400

    still practicing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import './App.css';
 
-function Welcome(props: any): JSX.Element {
-  return <h1>Hello, {props.name}</h1>
-}
-
 
 function App() {
   return (
-    <div>
-      <Welcome name='sara'/>
-    </div>
+    <div>hi</div>
   )
 }
 
