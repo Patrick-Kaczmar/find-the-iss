761b76384d8ee56a9dc1a86890ad7edf01a57886
Patrick Kaczmar <pk1blue@yahoo.com>
2022-07-18 22:01:52 -0400

    Changed to a space app i guess

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from "react";
-import './app.css'
-import Navigation from "./navigation/Navigation";
-import Cart from "./cart/Cart";
 
 function App() {
 
-  fetch('https://fakestoreapi.com/products')
-  .then( res => res.json())
-  .then( response => console.log(response))
+  fetch("https://api.wheretheiss.at/v1/satellites/25544")
+  .then(res => res.json())
+  .then(data => console.log(data))
 
   return (
     <>
-      <Navigation />
-      <Cart />
+      
     </>
-  );
+  )
 }
 
 export default App;
